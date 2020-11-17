53dd0dfcecb377006bc73d3349c254b4b502093e
Zihadul Islam <ahmedxihad@gmail.com>
2020-11-17 22:27:41 +0600

    user signup

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/order");
+const userRoutes = require("./api/routes/user");
 
 mongoose.set("useNewUrlParser", true);
 mongoose.set("useFindAndModify", false);
@@ -37,7 +38,7 @@ app.use((req, res, next) => {
 //Middleware
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
-
+app.use("/user", userRoutes);
 //handling error
 app.use((req, res, next) => {
   const error = new Error("Not found");
